4daee3d61d82d568e40734a115888a3098df6cc4
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-12 11:10:13 -0400

    Address tests

--- src/test/java/io/zipcoder/model/AddressTest.java
+++ src/test/java/io/zipcoder/model/AddressTest.java
@@ -15,11 +15,43 @@ public class AddressTest {
     }
 
     @Test
-    public void addressIdTest() {
+    public void addressStreet_numberTest() {
         Address address = new Address();
-        address.setId(1);
-        Integer expected = 1;
-        Assert.assertEquals(expected, address.getId());
+        address.setStreet_number("555");
+        String expected = "555";
+        Assert.assertEquals(expected, address.getStreet_number());
+    }
+
+    @Test
+    public void addressStreet_nameTest() {
+        Address address = new Address();
+        address.setStreet_name("Highfive Drive");
+        String expected = "Highfive Drive";
+        Assert.assertEquals(expected, address.getStreet_name());
+    }
+
+    @Test
+    public void addressCityTest() {
+        Address address = new Address();
+        address.setCity("Here");
+        String expected = "Here";
+        Assert.assertEquals(expected, address.getCity());
+    }
+
+    @Test
+    public void addressStateTest() {
+        Address address = new Address();
+        address.setState("Here");
+        String expected = "Here";
+        Assert.assertEquals(expected, address.getState());
+    }
+
+    @Test
+    public void addressZipTest() {
+        Address address = new Address();
+        address.setZip("19901");
+        String expected = "19901";
+        Assert.assertEquals(expected, address.getZip());
     }
 
 }
