41db69e687cb07cb4231a41f4433c898b770034e
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-04-12 14:21:38 -0400

    worked on bills

--- src/main/java/io/zipcoder/bank/service/BillService.java
+++ src/main/java/io/zipcoder/bank/service/BillService.java
@@ -21,10 +21,18 @@ public class BillService {
         return billRepository.save(bill);
     }
 
-    public Bill findBillById(Integer id) {
+    public Bill findBillByBillId(Integer id) {
         return billRepository.findOne(id);
     }
 
+    public Bill updateBillByBillId(Integer id, Bill bill) {
+        bill.setId(id);
+        return billRepository.save(bill);
+    }
+
+    public void deleteBillByBillId(Integer id) {
+        billRepository.delete(id);
+    }
 //    public Collection<Bill> findAllBillsByAccount(Integer id) {
 //        Collection collection = new ArrayList();
 //        Account account = new Account();
@@ -32,5 +40,9 @@ public class BillService {
 //        billRepository.findAll();
 //        collection.add()
 //        return collection;
-//    }
+//  }
+
+
+
+
 }
