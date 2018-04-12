bbfe24ae8dfdb06db6d141d41b17094fd4b3d4ef
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-12 08:51:28 -0400

    Customer model test

---
+++ src/test/java/io/zipcoder/model/BillTest.java
@@ -0,0 +1,4 @@
+package io.zipcoder.model;
+
+public class BillTest {
+}
---
+++ src/test/java/io/zipcoder/model/CustomerTest.java
@@ -0,0 +1,43 @@
+package io.zipcoder.model;
+
+import io.zipcoder.bank.model.Address;
+import io.zipcoder.bank.model.Customer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void customerIdTest() {
+        Customer customer = new Customer();
+        customer.setId(1234);
+        Integer expected = 1234;
+        Assert.assertEquals(expected, customer.getId());
+    }
+
+    @Test
+    public void customerFirstNameTest() {
+        Customer customer = new Customer();
+        customer.setFirst_name("Joe");
+        String expected = "Joe";
+        Assert.assertEquals(expected, customer.getFirst_name());
+    }
+
+    @Test
+    public void customerLastNameTest() {
+        Customer customer = new Customer();
+        customer.setLast_name("Joe");
+        String expected = "Joe";
+        Assert.assertEquals(expected, customer.getLast_name());
+    }
+
+    @Test
+    public void customerAddressTest() {
+        Customer customer = new Customer();
+        Address customerAddress = new Address();
+        customer.setAddress(customerAddress);
+        Address expected = new Address();
+        Assert.assertEquals(expected.getClass(), customer.getAddress().getClass());
+    }
+
+}
